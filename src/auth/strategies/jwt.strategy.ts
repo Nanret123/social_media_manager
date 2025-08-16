37f012a27476adbfbbf595eb42e9ccfdc4a834ca
Nanret123 <nanretgungshik@gmail.com>
2025-08-16 14:44:09 +0100

    feat: add guards and strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,39 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private configService: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_ACCESS_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        emailVerified: true,
+        accountStatus: true,
+      },
+    });
+
+    if (!user || user.accountStatus !== 'ACTIVE') {
+      throw new UnauthorizedException('User not found or inactive');
+    }
+
+    return user;
+  }
+}
